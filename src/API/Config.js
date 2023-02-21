573cbe268aec40fc5071a9b088a76c3182327d64
shaxzod <shaxzodaliyorov0202@gmail.com>
2023-02-21 17:31:10 +0500

    success

--- src/API/Config.js
+++ src/API/Config.js
@@ -1,2 +1,2 @@
 import axios from "axios";
-export const API = axios.create({ baseURL: 'https://shaxzod-eduction.onrender.com/api' })
+export const API = axios.create({ baseURL: 'https://shaxzod-eduction-server.onrender.com/api' })
